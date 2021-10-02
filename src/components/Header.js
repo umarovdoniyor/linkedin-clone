ae9d6378e668d8ca00949fc148363e218db84d6a
Doniyor Umarov <doniyorumarov@Doniyors-MacBook-Pro.local>
2021-10-02 09:30:46 +0900

    Header complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,6 +49,25 @@ const Header = (props) => {
                 <span>Notifications</span>
               </a>
             </NavList>
+            <User>
+              <a>
+                <img src='/images/user.svg' alt='user logo' />
+                <span>Me</span>
+                <img src='/images/down-icon.svg' alt='down icon' />
+              </a>
+              <SignOut>
+                <a>Sign Out</a>
+              </SignOut>
+            </User>
+            <Work>
+              <a>
+                <img src='/images/nav-work.svg' alt='work logo' />
+                <span>
+                  Work
+                  <img src='/images/down-icon.svg' alt='down icon' />
+                </span>
+              </a>
+            </Work>
           </NavListWrap>
         </Nav>
       </Content>
@@ -182,6 +201,46 @@ const NavList = styled.li`
       }
     }
   }
+`;
+
+const SignOut = styled.div`
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
+`;
+
+const User = styled(NavList)`
+  a > svg {
+    width: 24px;
+    border-radius: 50%;
+  }
+  a > img {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+  }
+  span {
+    display: flex;
+    align-items: center;
+  }
+  &:hover {
+    ${SignOut} {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+`;
+
+const Work = styled(User)`
+  border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
 export default Header;
