de470c30969c37199e5051d620c588fb7b2dd1cf
Doniyor Umarov <doniyorumarov@Doniyors-MacBook-Pro.local>
2021-10-03 18:38:34 +0900

    Main part, port component complete

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,6 +27,71 @@ const Main = (props) => {
           </button>
         </div>
       </ShareBox>
+      <div>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src='/images/user.svg' alt='user' />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img src='/images/ellipsis.svg' alt='ellipsis' />
+            </button>
+          </SharedActor>
+          <Description>Description </Description>
+          <SharedImg>
+            <a>
+              <img src='/images/shared-image.jpg' alt='shared-image' />
+            </a>
+          </SharedImg>
+          <SocialCounts>
+            <li>
+              <button>
+                <img
+                  src='https://static-exp1.licdn.com/sc/h/8ekq8gho1ruaf8i7f86vd1ftt'
+                  alt='LIKE'
+                />
+
+                <img
+                  src='https://static-exp1.licdn.com/sc/h/b1dl5jk88euc7e9ri50xy5qo8'
+                  alt='PRAISE'
+                />
+
+                <img
+                  src='https://static-exp1.licdn.com/sc/h/cpho5fghnpme8epox8rdcds22'
+                  alt='EMPATHY'
+                />
+                <span>75</span>
+              </button>
+            </li>
+            <li>
+              <a>2 comments</a>
+            </li>
+          </SocialCounts>
+          <SocialActions>
+            <button>
+              <img src='/images/like-icon.svg' alt='like-icon' />
+              <span>Like</span>
+            </button>
+            <button>
+              <img src='/images/comments-icon.svg' alt='comments-icon' />
+              <span>Comments</span>
+            </button>
+            <button>
+              <img src='/images/share-icon.svg' alt='share-icon' />
+              <span>Share</span>
+            </button>
+            <button>
+              <img src='/images/send-icon.svg' alt='send-icon' />
+              <span>Send</span>
+            </button>
+          </SocialActions>
+        </Article>
+      </div>
     </Container>
   );
 };
@@ -101,4 +166,126 @@ const ShareBox = styled(CommonCard)`
     }
   }
 `;
+
+const Article = styled(CommonCard)`
+  background: white;
+  color: black;
+
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+`;
+
+const SharedActor = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  padding-right: 40px;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+
+  a {
+    display: flex;
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  button {
+    position: absolute;
+    top: 0;
+    right: 12px;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+
+const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const SocialCounts = styled.ul`
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  list-style: none;
+
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+
+    button {
+      display: flex;
+    }
+  }
+`;
+
+const SocialActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 0;
+  min-height: 40px;
+  padding: 4px 8px;
+  button {
+    display: inline-flex;
+    align-items: center;
+    padding: 8px;
+    color: #0a66c2;
+
+    @media (min-width: 768px) {
+      span {
+        margin-left: 8px;
+      }
+    }
+  }
+`;
+
 export default Main;
